a186979bc3461a60b6465fd9b711ae23cce50ba2
sandonemaki <kamuramaki@gmail.com>
2022-11-20 00:17:15 +0900

    user に紐づく book の保存しても user の index が更新されない問題を解決した

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,10 @@ class Book < ApplicationRecord
 
   belongs_to :user
   after_touch :index!
+  after_save :user_save
+  def user_save
+    User.reindex!
+  end
 
   meilisearch {
     attribute :title, :price
